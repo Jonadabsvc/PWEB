f845b0f01c3184631dad267ab426073b4866a756
Jonadab <82825990+Jonadabsvc@users.noreply.github.com>
2022-11-13 14:06:46 -0300

    Add files via upload

---
+++ Atividade10/Atividade10.js
@@ -0,0 +1,15 @@
+var Aluno1 = new Object();
+Aluno1.ra = "0030482111044"
+Aluno1.nome = "Jonadab Santana Vieira Cerqueira"
+alert("ALUNO: " + Aluno1.nome + " RA: " + Aluno1.ra); 
+
+var Aluno2 = {
+    ra:"0030123456789",
+    nome:"Fulano de tal"
+};
+alert("ALUNO: " + Aluno2.nome + " RA: " + Aluno2.ra); 
+
+var Aluno3 = {};
+Aluno3.nome = "Fulano do teste";
+Aluno3.ra = "0030246810120";
+alert("ALUNO: " + Aluno3.nome + " RA: " + Aluno3.ra); 
